3564b43b44c47890f13cf34d790b154f300eaa52
JawaraGordon <jawaragordon@icloud.com>
2022-07-28 18:08:45 -0400

    072822

--- 07-data-structures-p2/algos.js
+++ 07-data-structures-p2/algos.js
@@ -2,9 +2,46 @@ console.log("good luck, you've got this!!", "🍀")
 
 // Given an array [2, 9, 1, 3] create a custom method/function to sort the array without using a built in sort method such as .sort.
 
+// are there any other methods that are off limits
+// can I use MDN
 
-// - Write the question down and confirm your assumptions  
+
+// - Write the question down and confirm your assumptions 
+// ask about constraints: how large, include duplicates
 //  - Write out/explain steps for the solution before coding 
 //  - Code out solution 
 //  - Run test case examples 
 //  - Give Big - O
+
+// [2, 9, 1, 3]
+// [1, 2, 3, 9]
+
+function sort(array){
+    for (let i = 0; i < array.length; i++) {
+        console.log("starter array", array)
+        for (let j = i + 1; j < array.length; j++) {
+        // console.log("current i", array[i], "current j", array[j])
+        if (array[i] > array[j]){
+            [array[i], array[j]] = array[[j], array[i]];
+        }
+    }
+}
+return array
+}
+
+// create new array = []
+// while loop
+// function newSort(arr){
+//     let array = []
+// let smallest = Math.min(...arr)
+// arr.indexOf(smallest)
+// .splice()
+
+// }
+
+
+// let temp = array[i]
+// array[i] = array[j]
+// array[i] = temp
+
+console.log(sort([2,9,1,3]))
--- 07-data-structures-p2/linked_list.rb
+++ 07-data-structures-p2/linked_list.rb
@@ -3,6 +3,7 @@ require 'pry'
 # 1. a method that prints the value of every node and
 # 2. a method that inserts a node and then prints the list
 
+# we're doing OOP should we create a node Class and linked list class
 
 # - Write the question down and confirm your assumptions  
 #  - Write out/explain steps for the solution before coding 
@@ -10,6 +11,43 @@ require 'pry'
 #  - Run test case examples 
 #  - Give Big - O
 
+class Node 
+#value and next
+attr_accessor :value, :next
 
+#can be shared with other methods
+def initialize(value)
+    @value = value
+    @next = nil
+end
+
+
+end
+
+# head 
+# 1 -> 2 -> 3 -> nil
+
+class LinkedList
+#head length 
+
+attr_accessor :head, :length
+
+def initialize
+    @head = nil
+    @length = 0
+end 
+
+def unshift(value)
+    new_node = Node.new(value)
+    temp = self.head
+    self.head = new_node
+    self.head.next = temp
+    self.length +=1
+    self
+end
+
+end
+
+ll = LinkedList.new
 
 binding.pry
